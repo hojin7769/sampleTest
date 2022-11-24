9c983fea434165db742d399021f7b0e66cc62082
hojin7769 <sungsill150@gmail.com>
2022-11-24 14:11:05 +0900

    init table test

---
+++ src/main/groovy/com/sample/sampletest/SampleTestApplication.groovy
@@ -0,0 +1,13 @@
+package com.sample.sampletest
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SampleTestApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SampleTestApplication, args)
+    }
+
+}

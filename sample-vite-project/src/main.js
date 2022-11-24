40fb5bfc4c5e5d8e18dc58d086bfd1c0823bac86
hojin7769 <sungsill150@gmail.com>
2022-11-24 15:53:44 +0900

    feature init project 프로젝트 세부사항 init

--- sample-vite-project/src/main.js
+++ sample-vite-project/src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from './router/index.js'
 
-createApp(App).mount('#app')
+import { createPinia } from 'pinia'
+
+const pinia = createPinia()
+
+createApp(App).use(pinia).use(router).mount('#app')
